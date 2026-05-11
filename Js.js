419c3b08e37bb470d9a86695911ba233c414659b
Buddha Pranathi devi <24nu5a0406@nsrit.edu.in>
2026-05-11 08:21:02 +0530

    Create Js.js

---
+++ Js.js
@@ -0,0 +1,17 @@
+function checkPassword(){
+
+var password = document.getElementById("password").value;
+
+if(password === "123"){
+
+window.location.href = "wish.html";
+
+}
+
+else{
+
+alert("Wrong Password");
+
+}
+
+}
